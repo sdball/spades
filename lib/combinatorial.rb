7444be1873271d87d2bb6a6cd0ad254cddf3f951
Stephen Ball <sdball@gmail.com>
2011-11-05 02:24:13 -0400

    handling 0! and -n!; renamed choose to combinatorial

--- lib/combinatorial.rb
+++ lib/combinatorial.rb
@@ -1,11 +1,14 @@
 module Math
   def self.factorial(n)
+    return 1 if 0 >= n
     return n if 1 == n
     return n if 2 == n
     (2..n).inject(:*)
   end
 
-  def self.choose(x,y)
+  def self.combinatorial(x,y)
+    return 0 if y > x
+    return 1 if x == y
     factorial(x) / (factorial(y) * factorial(x - y))
   end
 end
--- test/combinatorial.rb
+++ test/combinatorial.rb
@@ -3,6 +3,8 @@ require './lib/combinatorial.rb'
 class TestCombinatorial < MiniTest::Unit::TestCase
   def test_factorial
     known_factorials = {
+      -4 => 1,
+      0 => 1,
       1 => 1,
       2 => 2,
       3 => 6,
@@ -14,9 +16,28 @@ class TestCombinatorial < MiniTest::Unit::TestCase
     end
   end
 
+  def test_combinatorial
+    known_combinatorials = {
+      [3,1] => 3,
+      [10,2] => 45,
+      [12,6] => 924
+    }
+    known_combinatorials.each_pair do |operands, result|
+      assert_equal(result, Math.combinatorial(operands[0], operands[1]))
+    end
+  end
+
+  # benchmarks, only run if tests pass
   def bench_factorial
     assert_performance_linear do |n| # n is a range value
       Math.factorial(n)
     end
   end
+
+  def bench_combinatorial
+    assert_performance_linear do |n| # n is a range value
+      Math.combinatorial(n,6)
+    end
+  end
+
 end
