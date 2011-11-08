a120abc2f7acd28427e28f093c016df9bfda75d3
Stephen Ball <sdball@gmail.com>
2011-11-07 20:59:51 -0500

    comments and argument check for spades/game; deal method in spades/game

--- lib/spades/game.rb
+++ lib/spades/game.rb
@@ -1,6 +1,32 @@
 module Spades
+  ##
+  # Given four Spades::Cards, return the winning card
+  #
+  # 2c = Spades::Card.new('c2')
+  # 3c = Spades::Card.new('c2')
+  # 4c = Spades::Card.new('c2')
+  # 5c = Spades::Card.new('c2')
+  # Spades.winning_card(2c, 3c, 4c, 5c)
+  # => 5c
   def self.winning_card(*trick)
     trick = trick[0] if trick[0].is_a? Array
+    raise ArgumentError, "Trick must have four cards." unless 4 == trick.length
     trick.sort.last
+  end
+
+  ##
+  # Given an array of Spades::Cards return it as four arrays.
+  #
+  # deck = Spades.deck(52)
+  # player1, player2, player3, player4 = Spades.deal(deck)
+  def self.deal(deck)
+    raise ArgumentError, "Deck must be divisible by four." unless 0 == deck.length % 4
+    deal = deck.length / 4
+    return [
+      deck.slice!(0, deal),
+      deck.slice!(0, deal),
+      deck.slice!(0, deal),
+      deck.slice!(0, deal)
+    ]
   end
 end
--- test/test_spades_game.rb
+++ test/test_spades_game.rb
@@ -11,6 +11,19 @@ class TestSpadesGame < MiniTest::Unit::TestCase
     assert_equal(Spades::Card.new('2s'), Spades.winning_card(trick))
   end
 
+  def test_invalid_trick_rejected
+    invalid_trick = create_cards('2c', '3c', '2h')
+    assert_raises(Spades::ArgumentError) { Spades.winning_card(invalid_trick) }
+  end
+
+  def test_dealing_cards
+    deck = Spades.deck(16)
+    hands = Spades.deal(deck)
+    hands.each do |hand|
+      assert_equal 4, hand.length
+    end
+  end
+
   def create_cards(*cards)
     cards.map { |card| Spades::Card.new(card) }
   end
